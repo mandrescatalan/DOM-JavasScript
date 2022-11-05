0ac73677e9fc99ab2d49a5200917d99c50e6775e
mandrescatalan <mandrescatalan@unicesar.edu.co>
2022-11-05 00:32:34 -0500

    clase02

--- script.js
+++ script.js
@@ -19,8 +19,21 @@ const btn = document.querySelector("[data-form-btn]");
 //Funcion anonimas
 const createTask = (evento)=>{
     evento.preventDefault();
-    const input = document.querySelector("[data-from-input]")
-    console.log(input.value);
+    const input = document.querySelector("[data-from-input]");
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = '';
+    //backticks
+    const content = `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+        </div>
+    <i class="fas fa-trash-alt trashIcon icon"></i>`
+
+    task.innerHTML = content;
+    list.appendChild(task);
 }
 
 btn.addEventListener("click",createTask);
